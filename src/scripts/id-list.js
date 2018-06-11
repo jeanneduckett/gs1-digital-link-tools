47f990f655f9c9974eaf6cf5dea596616b94a238
C-D-Lewis <Bonsitm@gmail.com>
2018-06-11 11:46:08 +0100

    Add building of digital link string and QR code download

---
+++ src/scripts/id-list.js
@@ -0,0 +1,16 @@
+var ID_LIST = [
+  { "ai": "00", "label": "Serial Shipping Container Code (SSCC)" },
+  { "ai": "01", "label": "Global Trade Item Number (GTIN)" },
+  { "ai": "253", "label": "Global Document Type Identifier (GDTI)" },
+  { "ai": "255", "label": "Global Coupon Number (GCN)" },
+  { "ai": "401", "label": "Global Identification Number for Consignment (GINC)" },
+  { "ai": "402", "label": "Global Shipment Identification Number (GSIN)" },
+  { "ai": "414", "label": "Identification of a physical location - Global Location Number" },
+  { "ai": "415", "label": "Global Location Number of the invoicing party" },
+  { "ai": "8003", "label": "Global Returnable Asset Identifier (GRAI)" },
+  { "ai": "8004", "label": "Global Individual Asset Identifier (GIAI)" },
+  { "ai": "8006", "label": "Identification of an individual trade item piece" },
+  { "ai": "8010", "label": "Component/Part Identifier (CPID)" },
+  { "ai": "8017", "label": "Global Service Relation Number - Provider" },
+  { "ai": "8018", "label": "Global Service Relation Number - Recipient" }
+];
--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,22 @@
-const MAX_LABEL_LENGTH = 48;
+const MAX_LENGTH = 48;
+const DEFAULT_GTIN_VALUE = '9780345418913';
+const QR_SIZE = 200;
+const QR_TIMEOUT = 500;
+const DOMAINS = {
+  TN_GG: { value: 'tngg', label: 'tn.gg (EVRYTHNG)', url: 'tn.gg' },
+  ID_GS1: { value: 'idgs1org', label: 'id.gs1.org (Canonical)', url: 'id.gs1.org' },
+  CUSTOM: { value: 'custom', label: 'Custom domain...', url: 'domain.example.org' },
+};
 
-const setVisible = (id, state) => document.getElementById(id).style.display = state ? 'block' : 'none';
+let digitalLink, qrFetchTimeout;
+
+const get = id => document.getElementById(id);
+
+const setVisible = (id, state) => get(id).style.display = state ? 'block' : 'none';
 
 const getInputIdFromGS1AI = ai => `input_gs1_attribute_${ai}`;
 
-const maxLength = str => (str.length < MAX_LABEL_LENGTH)
-  ? str 
-  : `${str.substring(0, MAX_LABEL_LENGTH - 2)}...`;
+const maxLength = str => (str.length < MAX_LENGTH) ? str : `${str.substring(0, MAX_LENGTH - 2)}...`;
 
 const addRowToTable = (table, item) => {
   const { label, ai } = item;
@@ -17,7 +27,7 @@ const addRowToTable = (table, item) => {
   valueCell.innerHTML = `<input id="${getInputIdFromGS1AI(ai)}" type="text" class="form-control ml-2">`;
 
   // Listen for value input
-  const rowInput = document.getElementById(getInputIdFromGS1AI(item.ai));
+  const rowInput = get(getInputIdFromGS1AI(item.ai));
   rowInput.oninput = () => {
     item.value = rowInput.value;
   };
@@ -37,18 +47,94 @@ const searchAiList = (query) => {
   return AI_LIST.filter(item => item.label.toLowerCase().includes(query) || item.ai === query);
 };
 
+const downloadFromQrServer = () => {
+  const url = `http://api.qrserver.com/v1/create-qr-code?data=${digitalLink}&size=${QR_SIZE}x${QR_SIZE}&format=png`;
+  get('img_qr_code').src = url;
+};
+
+const updateQrCode = () => {
+  // Don't request for every keystroke
+  if (qrFetchTimeout) clearTimeout(qrFetchTimeout);
+
+  qrFetchTimeout = setTimeout(() => {
+    qrFetchTimeout = null;
+
+    downloadFromQrServer();  
+  }, QR_TIMEOUT);
+};
+
+const updateDigitalLink = () => {
+  // Domain
+  const domainKey = Object.keys(DOMAINS)
+    .find(item => DOMAINS[item].value === get('select_domain').value);
+  const domain = DOMAINS[domainKey];
+  
+  // Identifier
+  const identifier = ID_LIST.find(item => item.ai === get('select_identifier').value);
+  const identifierValue = get('input_identifier_value').value;
+  digitalLink = `https://${domain.url}/${identifier.ai}/${identifierValue}`;
+
+  // Key qualifiers
+  const inputCPV = get('input_cpv');
+  if (inputCPV.value) digitalLink += `/22/${inputCPV.value}`;
+  const inputBatchOrLot = get('input_batch_lot');
+  if (inputBatchOrLot.value) digitalLink += `/10/${inputBatchOrLot.value}`;
+  const inputSerialNumber = get('input_serial_number');
+  if (inputSerialNumber.value) digitalLink += `/21/${inputSerialNumber.value}`;
+
+  // Update UI
+  get('span_digital_link').innerHTML = digitalLink;
+  updateQrCode();
+};
+
 const setupUI = () => {
-  const checkQualifiers = document.getElementById('check_key_qualifiers');
+  // Domain
+  const selectDomain = get('select_domain');
+  selectDomain.value = DOMAINS.ID_GS1.value;
+  selectDomain.onchange = () => {
+    setVisible('div_domain_wrapper', selectDomain.value === DOMAINS.CUSTOM.value);
+    updateDigitalLink();
+  };
+
+  const inputDomainValue = get('input_domain_value');
+  inputDomainValue.value = DOMAINS.CUSTOM.url;
+  inputDomainValue.oninput = () => {
+    DOMAINS.CUSTOM.url = inputDomainValue.value;
+    updateDigitalLink();
+  };
+
+  // Set identifier options
+  const selectIdentifier = get('select_identifier');
+  selectIdentifier.options.length = 0;
+  ID_LIST.forEach(item => selectIdentifier.options.add(new Option(item.label, item.ai)));
+  selectIdentifier.onchange = updateDigitalLink;
+  selectIdentifier.value = ID_LIST[1].ai;
+
+  const inputIdentifierValue = get('input_identifier_value');
+  inputIdentifierValue.oninput = updateDigitalLink;
+  inputIdentifierValue.value = DEFAULT_GTIN_VALUE;
+
+  // Data qualifiers
+  const checkQualifiers = get('check_key_qualifiers');
   checkQualifiers.onclick = () => setVisible('div_key_qualifiers_group', checkQualifiers.checked);
-  const checkGS1Attributes = document.getElementById('check_gs1_data_attributes');
+
+  const inputCPV = get('input_cpv');
+  inputCPV.oninput = updateDigitalLink;
+  const inputBatchOrLot = get('input_batch_lot');
+  inputBatchOrLot.oninput = updateDigitalLink;
+  const inputSerialNumber = get('input_serial_number');
+  inputSerialNumber.oninput = updateDigitalLink;
+
+  // GS1 Attributes
+  const checkGS1Attributes = get('check_gs1_data_attributes');
   checkGS1Attributes.onclick = () => setVisible('div_gs1_data_attributes_group', checkGS1Attributes.checked);
 
   // Add rows to GS1 Data Attributes table
-  const table = document.getElementById('table_gs1_data_attributes');
+  const table = get('table_gs1_data_attributes');
   AI_LIST.forEach(item => addRowToTable(table, item));
 
   // Setup attribute search
-  const inputSearchAiList = document.getElementById('input_search_ai_list');
+  const inputSearchAiList = get('input_search_ai_list');
   inputSearchAiList.oninput = () => {
     // Hide all, unless they have a value
     AI_LIST.forEach(item => setRowVisible(item.row, item.value));
@@ -63,4 +149,5 @@ const setupUI = () => {
   console.log('Script loaded!');
 
   setupUI();
+  updateDigitalLink();
 })();
